455453ffb4184825fa341d71ca204cd05190856e
Ruslan Latypov <stihyra@mail.ru>
2023-01-31 21:40:52 +0500

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,15 +7,16 @@ MAX = 5000
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_simply(question):  # Проверка числа
+    if question % 2 == 0:
+        return 'no'
+    for i in range(3, round(math.sqrt(question)), 2):
+        if question % i == 0:
+            return 'no'
+    return 'yes'
+
+
 def game5():  # Логика игры определить простое число
-    question = random.randint(1, 5000)
-    if question % 2 == 0:  # Проверка числа
-        corr = 'no'
-    else:
-        for i in range(3, round(math.sqrt(question)), 2):
-            if question % i == 0:
-                corr = 'no'
-                break
-        else:
-            corr = 'yes'
+    question = random.randint(MIN, MAX)
+    corr = is_simply(question)
     return question, corr
