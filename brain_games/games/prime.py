f471c8ecf226a8e088337bf31401addfdb5133a6
Ruslan Latypov <stihyra@mail.ru>
2023-02-09 21:15:00 +0500

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,13 +9,13 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_simply(question):  # Проверка числа
     if question % 2 == 0:
-        return True
+        return False
     for i in range(3, round(math.sqrt(question)), 2):
         if question % i == 0:
-            return True
-    return False
+            return False
+    return True
 
 
 def gives_answer_question():
     question = random.randint(MIN, MAX)
-    return str(question), 'yes' if is_simply else 'no'
+    return str(question), 'yes' if is_simply() else 'no'
