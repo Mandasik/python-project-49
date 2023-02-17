16f420a8f21d4dda015d64902e4450c9d188e413
Ruslan Latypov <stihyra@mail.ru>
2023-02-17 21:09:06 +0500

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,8 +10,6 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(number):
     if number % 2 == 0 or number % 3 == 0 or number <= 1:
         return False
-    elif number == 2:
-        return True
     for i in (divider for divider in range(3, ceil(sqrt(number)), 2)):
         if number % i == 0:
             return False
@@ -20,5 +18,5 @@ def is_prime(number):
 
 def gives_answer_question():
     question = random.randint(MIN, MAX)
-    answer = 'yes' if is_prime(question) else 'no'
+    answer = 'yes' if question == 2 or is_prime(question) else 'no'
     return str(question), answer
