2cfad61ade40f84166fa21d041e1bb41f1a50d53
Ruslan Latypov <stihyra@mail.ru>
2023-02-17 20:27:16 +0500

    fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,16 +8,16 @@ RULE = 'What is the result of the expression?'
 COMPARISON = {'+': operator.add, '*': operator.mul, '-': operator.sub}
 
 
-def calculate(numeric_a, numeric_b, operation):
-    answer = operation(numeric_a, numeric_b)
+def calculate(number_a, number_b, operation):
+    answer = operation(number_a, number_b)
     return answer
 
 
 def gives_answer_question():
-    numeric_a = random.randint(MIN, MAX)
-    numeric_b = random.randint(MIN, MAX)
+    number_a = random.randint(MIN, MAX)
+    number_b = random.randint(MIN, MAX)
     simbol = random.choice(list(COMPARISON.keys()))
     operation = COMPARISON[simbol]
-    answer = str(calculate(numeric_a, numeric_b, operation))
-    question = f'{numeric_a} {simbol} {numeric_b}'
+    answer = str(calculate(number_a, number_b, operation))
+    question = f'{number_a} {simbol} {number_b}'
     return question, answer
