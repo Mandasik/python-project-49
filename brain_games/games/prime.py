4675351c2b92ed8642c625cfba49a53813848699
Ruslan Latypov <stihyra@mail.ru>
2023-02-20 15:59:58 +0500

    fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,16 +1,15 @@
 import random
-from math import sqrt, ceil
 
 
 MIN = -100
-MAX = 4000
+MAX = 100
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    if number % 2 == 0 or number % 3 == 0 or number <= 1:
+    if number <= 1:
         return False
-    for i in (divider for divider in range(3, ceil(sqrt(number)), 2)):
+    for i in (divider for divider in range(2, number)):
         if number % i == 0:
             return False
     return True
@@ -18,5 +17,5 @@ def is_prime(number):
 
 def gives_answer_question():
     question = random.randint(MIN, MAX)
-    answer = 'yes' if question == 2 or is_prime(question) else 'no'
+    answer = 'yes' if is_prime(question) else 'no'
     return str(question), answer
