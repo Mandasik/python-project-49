ebedbf8215395a91d0beeb3cb0136e661449619a
Ruslan Latypov <stihyra@mail.ru>
2023-02-21 11:50:26 +0500

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,7 +3,7 @@ from math import sqrt, ceil
 
 
 MIN = -100
-MAX = 1000000
+MAX = 100000000
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
@@ -11,10 +11,10 @@ def is_prime(number):
     if number <= 1 or number != 2 and number % 2 == 0 \
             or number != 3 and number % 3 == 0:
         return False
-    gen_prime = (
+    potentially_prime = (
         divider for divider in range(4, ceil(sqrt(number)))
         if divider % 2 != 0 or divider % 3 != 0)
-    for i in gen_prime:
+    for i in potentially_prime:
         if number % i == 0:
             return False
     return True
